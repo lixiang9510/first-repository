fbaceea6a978aa1ab466cdc55095a16789aa6cb1
lixiang <2218226617@qq.com>
2018-11-23 10:26:41 +0800

    add js

---
+++ js/js-out.js
@@ -0,0 +1,9 @@
+
+function toChange(){
+	var obox=document.getElementById('box')
+			obox.style.width='300px';
+			obox.style.height='300px';
+			obox.style.backgroundColor='yellow';
+}
+var obox=document.getElementById('box')
+obox.onclick=toChange;
---
+++ js/js-outside-practice.js
@@ -0,0 +1,8 @@
+function tobig(){
+	var big=document.getElementById('box');
+		big.className='box1';
+}
+function recome(){
+	var come=document.getElementById('box')
+		come.className='box';
+}
