5ec0abd436448284402bbe1115fa9dfb371c983f
lixiang <2218226617@qq.com>
2018-12-11 20:59:48 +0800

    a

--- js/js-26通用动画.js
+++ js/js-26通用动画.js
@@ -1,30 +1,58 @@
 // alert('a');
 var timer=0;
-	function animal(obj,attr,target){
+	/*
+	注释：
+	object   	obj 		传入的对象
+	attr		属性
+	不传就是匀速，传了就是减速
+	*/
+	function animate(obj,attr,target,decelerate,fnEnd){
+		var ispeed=10;
 		clearInterval(obj.timer);
 		obj.timer=setInterval(function(){
-			var ispeed=7;
+			if(!decelerate){
+				ispeed=100;
+			}
 			var current=parseFloat(getComputedStyle(obj,false)[attr]);
 			if(attr=='opacity'){
 				current=Math.round(current*100);
-			}
-			ispeed=(target-current)*Math.abs(1/(target-current))*ispeed;
-			if(Math.abs(current-target)<=Math.abs(ispeed)){
-				if(attr=='opacity'){
-					obj.style[attr]=target/100;
-				}else{
-					obj.style[attr]=target+'px';
-				}
-				clearInterval(obj.timer);
 			}else{
-				if(attr=='opacity'){
-					obj.style[attr]=(current+ispeed)/100;
-				}else{
-					obj.style[attr]=current+ispeed+'px';
-				}	
+				current=Math.round(current);
 			}
-		},10)
+			if(decelerate){
+				if(Math.abs(ispeed)>1){
+					ispeed=(target-current)/10;
+				}else{
+					ispeed=(target-current)*Math.abs(1/(target-current));
+				}
+				if(!ispeed){
+					clearInterval(obj.timer);
+					if(fnEnd){
+						fnEnd();
+					}	
+				}
+			}else{
+				ispeed=(target-current)*Math.abs(1/(target-current))*Math.abs(ispeed);
+				if(Math.abs(current-target)<=Math.abs(ispeed)){
+					if(attr=='opacity'){
+						obj.style[attr]=target/100;
+					}else{
+						obj.style[attr]=target+'px';
+					}
+						clearInterval(obj.timer);
+						if(fnEnd){
+							fnEnd();
+						}	
+				}
+			}
+			if(attr=='opacity'){
+				obj.style[attr]=(current+ispeed)/100;
+			}else{
+				obj.style[attr]=current+ispeed+'px';
+			}
+			console.log(ispeed,current);
+		},30)
 	}
-	function compatible(compati){
-		var compati=document.documentElement.scrollTop||document.body.scrollTop;
+	function compatible(com){
+		 com=document.documentElement.scrollTop||document.body.scrollTop;
 	}
