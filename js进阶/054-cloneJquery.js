bca13369f35944285f9c1eb8814f0b3c75fa8d9a
lixiang <2218226617@qq.com>
2019-02-26 20:55:46 +0800

    a

--- js进阶/054-cloneJquery.js
+++ js进阶/054-cloneJquery.js
@@ -40,25 +40,64 @@
 			}
 			
 			//4.数组
+			else if(kQuery.isArray(selector)){
+				for(var i=0;i<selector.length;i++){
+					this[i]=selector[i];
+				}
+				this.length=selector.length;
+			}
 			//4.1 真数组
 			//4.2 假数组
 			//5.对象
+			else{
+				this[0]=selector;
+				this.length=1;
+			}
 			//5.1普通对象
 			//5.2 Dom对象
-			
-			
+		},
+		get:function(num){
+			if(num){
+				if(kQuery.isNumber(num)){
+					if(num>=0){
+						return this[num];
+					}else{
+						return this[this.length+num];
+					}
+				}
+			}else{
+				var arr = [];
+				for(var i=0;i<this.length;i++){
+					arr.push(this[i]);
+				}
+				return arr;
+			}
 		}
 
 	}
-	kQuery.isFunction=function(fn){
-		return typeof fn=='function';
+	kQuery.fn.extent=kQuery.extent=function(options){
+		for(key in options){
+			this[key]=options[key];
+		}
 	}
-	kQuery.isString=function(str){
-		return typeof str=='string';
-	}
-	kQuery.isHtml=function(str){
-		return /<[^<>]+>/.test(str);
-	}
+	kQuery.extent({
+		isFunction : function(str){
+			return typeof str=='function';
+		},
+		isString : function(str){
+			return typeof str=='string';
+		},
+		isHtml : function(str){
+			return /<[^<>]+>/.test(str);
+		},
+		isArray : function(str){
+			return typeof str=='object' && length in str;
+		},
+		isNumber : function(str){
+			return typeof str =='number'
+		}
+	})
+
 	kQuery.fn.init.prototype=kQuery.fn; 
 	window.$=window.kQuery=kQuery;
 })(window);
