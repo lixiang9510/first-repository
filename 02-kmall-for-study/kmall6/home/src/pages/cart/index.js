0131fc3218500c298692050ccc17e45feb830ee0
lixiang <2218226617@qq.com>
2019-04-30 17:02:44 +0800

    a

--- 02-kmall-for-study/kmall6/home/src/pages/cart/index.js
+++ 02-kmall-for-study/kmall6/home/src/pages/cart/index.js
@@ -12,34 +12,123 @@ require('./index.css')
 var _util = require('util')
 var _product = require('service/product')
 var _cart = require('service/cart')
+var _nav = require('pages/common/nav')
 var tpl = require('./index.tpl')
 var page = {
 	init:function(){
 		this.$elem = $('.cart-box');
+		this.totalCartPrice = 0;
 		this.loadCart();
 		this.bindEvent();
 	},
-	loadCart:function(){
-		var _this = this;
+	renderCart:function(){
+		_nav.loadCartCound();
 		_cart.getCart(function(cart){
 			if(cart.cartList.length>0){
 				//处理图片
+				this.totalCartPrice = cart.totalCartPrice;
 				cart.cartList.forEach(function(item){
 					item.product.mainImg = item.product.images.split(',')[0]
 				})
-				console.log(cart);
 				var html = _util.render(tpl,cart);
 				_this.$elem.html(html);
 			}else{
 				_this.$elem.html('<p class="empty-msg">购物车空空如也!</p>')	
 			}
-		},function(){
-			_this.$elem.html('<p class="empty-msg">加载购物车失败!稍后再试</p>')	
 		})
-
 	},
 	bindEvent:function(){
 		var _this = this;
+		this.$elem.on('click','select-one',function(){
+			var $this = $('this');
+			var productId = $this.parents('.paoduct-item').data('product-id');
+			if($this.is(":checked")){
+				_cart.selectOne({productId:productId},function(cart){
+					_this.renderCart(cart);
+				},function(msg){
+					_util.showErrorMsg(msg)
+				})
+			}else{
+				_cart.selectOne({productId:productId},funcction(cart){
+					_this.renderCart(cart);
+				},function(msg){
+					_util.showErrorMsg(msg)
+				})
+			}
+		})
+		if(cart.cartList.length>0){
+			cart.cartList.forEach(function(item){
+				// item.product.maining = item.product.images.split(',')[0]
+			})
+			this.$elem.on('click','select-all',function(){
+				var $this = $(this);
+				if($this.is(":checked")){
+					_cart.selectAll(function(cart){
+						_this.renderCart(cart)
+					},function(msg){
+						_util.showErrorMsg(msg)
+					})
+				}else{
+					_cart.unselectAll(function(cart){
+						_this.renderCart(cart)
+					},function(msg){
+						_util.showErrorMsg(msg)
+					})
+				}
+			})
+			this.$else.on('click','delete-one',function(){
+				if(_util.confirm('您确定要删除该件商品嘛？')){
+					var $this = $(this);
+					var productId = $this.parents('.product-item').data('product-id')
+					_cart.deleteOne({productId:productId},function(cart){
+						_util.renderCart(cart)
+					},function(msg){
+						_util.showErrorMsg(msg)
+					})
+				}
+			})
+			this.$elem.on('click','delete-selected',function(){
+				if(_util.confirm('您确定要删除您所选中的所有商品嘛？'),function(cart){
+					_util.renderCart(cart)
+				},function(msg){
+					_util.showErrorMsg(msg)
+				})
+			})
+			this.$elem.on('click','.count-btn',function(){
+				var $this = $(this);
+				var productId = $this.parents('.product-item').data('product-id');
+				var $input = $this.siblings('.count-input');
+				var stock = $input.data('stock');
+				var current = parseInt($input.val());
+				var newCount = 0;
+				if($this.hasClass('plus')){
+					if(current == stock){
+						_util.showErrorMsg('商品数量已经超出库存了');
+						return ;
+					}
+					newCount = newCount +1;
+				}
+				else if($this.hasClass('minus')){
+					if(current == 1){
+						_util.showErrorMsg('商品数量最少为一个');
+						return;
+					}
+					newCount = newCount - 1				
+				}
+				_util.updateCount({productId:productId},function(cart){
+					_this.renderCart(cart);
+				},function(msg){
+					_util.showErrorMsg(msg)
+				})
+			})
+			this.$elem.on('click','.btn-submit',function(){
+				if(_this.totalCartPrice > 0){
+					window.location.href = './order-confirm.html'
+				}else{
+					_util.showErrorMsg('请选择购物车信息后再提交')
+				}
+			})
+		}
 	}
 
 }
--- 02-kmall-for-study/kmall6/home/src/service/cart/index.js
+++ 02-kmall-for-study/kmall6/home/src/service/cart/index.js
@@ -30,6 +30,66 @@ var _cart = {
 			error:error			
 		})			
 	},
+	selectOne:function(data,success,error){
+		_util.request({
+			url:'./cart/selectOne',
+			method:'put',
+			data:'data',
+			success:success,
+			error:error
+		})
+	},
+	unselectOne:function(data,success,error){
+		_util.request({
+			url:'./uncart/selectOne',
+			method:'put',
+			data:data
+			success:success,
+			error:error
+		})
+	},
+	selectAll:function(success,error){
+		_util.request({
+			url:'./uncart/selectAll',
+			method:'put',
+			success:success,
+			error:error
+		})
+	},
+	unselectAll:function(data,success,error){
+		_util.request({
+			url:'./uncart/selectOne',
+			method:'put',
+			success:success,
+			error:error
+		})
+	},
+	deleteOne:function(data,success,error){
+		_util.request({
+			url:'./cart/deleteOne',
+			method:'put',
+			data:data,
+			success:success,
+			error:error
+		})
+	},
+	deleteSelected:function(success,error){
+		_util.request({
+			url:'./cart/deleteOne',
+			method:'put',
+			success:success,
+			error:error
+		})
+	},
+	updateCount:function(data,success,error){
+		_util.request({
+			url:'/cart/updateCount',
+			method:'put',
+			data:data,
+			success:success,
+			data:data
+		})
+	}
 }
 
 module.exports = _cart;
